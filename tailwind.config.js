0b0d45adff49bc751ecaab940c1f5348697a7336
dani.Madmolil <dani.madmolil@gmail.com>
2023-05-04 02:56:39 +0330

    extend screen sizes of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        800: "800px",
+        900: "900px",
+        1200: "1200px",
+        1400: "1400px",
+        1600: "1600px",
+        1700: "1700px",
+        1920: "1920px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
